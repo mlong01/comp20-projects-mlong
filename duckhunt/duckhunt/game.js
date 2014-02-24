8f6e0d288f581d5c494cec958d5f6cf17d70107f
Matt Long <minnvik98151@gmail.com>
2014-02-23 23:17:51 -0500

    Added rest of ducks and dog; still need ground

--- duckhunt/duckhunt/game.js
+++ duckhunt/duckhunt/game.js
@@ -16,7 +16,7 @@ function draw() {
 		//tree
 		ctx.drawImage(img, 0,   270, 80,  130, 30,  150, 170, 300);
 		//bushes - done after tree so that tree is behind grass
-		ctx.drawImage(img, 0,   700, 900, 200, 0,   350, 800, 200);
+		ctx.drawImage(img, 0,   700, 900, 200, 0,   370, 800, 200);
 		//duck 1
 		ctx.drawImage(img, 212, 198, 30,  30,  500, 100, 50,  50);
 		//duck 2
@@ -24,9 +24,11 @@ function draw() {
 		//duck 3
 		ctx.drawImage(img, 48,  237, 30,  30,  650, 300, 50,  50);
 		//duck 4
+		ctx.drawImage(img, 130, 121, 34,  30,  50,  50,  55,  50);
 		//duck 5
+		ctx.drawImage(img, 83,  157, 30,  30,  670, 50,  50,  50);
 		//dog
-
+		ctx.drawImage(img, 66,  1,   50,  42,  200, 510, 80,  70);
 
 
 	} else {
