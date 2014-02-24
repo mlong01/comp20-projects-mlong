9a890449a04ba460ab90b7b77de8846862882e19
Matt Long <minnvik98151@gmail.com>
2014-02-23 22:55:04 -0500

    Added two ducks

--- duckhunt/duckhunt/game.js
+++ duckhunt/duckhunt/game.js
@@ -14,11 +14,18 @@ function draw() {
 	
 
 		//tree
-		ctx.drawImage(img, 0, 270, 80,  130, 30, 150, 170, 300);
-		//bushes - done after tree so that it's behind grass
-		ctx.drawImage(img, 0, 700, 900, 200, 0, 350, 800, 200);
+		ctx.drawImage(img, 0,   270, 80,  130, 30,  150, 170, 300);
+		//bushes - done after tree so that tree is behind grass
+		ctx.drawImage(img, 0,   700, 900, 200, 0,   350, 800, 200);
 		//duck 1
-		ctx.drawImage(img, 212, 198, 30, 30, 500, 100, 50, 50);
+		ctx.drawImage(img, 212, 198, 30,  30,  500, 100, 50,  50);
+		//duck 2
+		ctx.drawImage(img, 301, 158, 30,  30,  220, 200, 50,  50);
+		//duck 3
+		ctx.drawImage(img, 48,  237, 30,  30,  650, 300, 50,  50);
+		//duck 4
+		//duck 5
+		//dog
 
 
 
