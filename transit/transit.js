42b39d5fb286acc83eabccd77fcc074a1b99fa7d
Matt Long <minnvik98151@gmail.com>
2014-03-13 02:58:15 -0400

    Attempting Geolocation services

--- transit/transit.js
+++ transit/transit.js
@@ -1,3 +1,25 @@
-function makeMap() {
-	console.log("Heya");
+
+function makeMap(position) {
+
+	//Find Lat + lng with geolocation, then do 
+	var crd = position.coords;
+	var lng = crd.longitude;
+	var lat = crd.latitude;
+
+	console.log(lat + " " + lng);
+
+	var mapOptions = {
+		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom: 12
+	};
+
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+								  mapOptions);
 }
+
+function initialize() {
+	navigator.geolocation.getCurrentPosition(makeMap);
+}
+
+
+google.maps.event.addDomListener(window, 'load', makeMap);
