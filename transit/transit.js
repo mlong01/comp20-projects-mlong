2a4ce33046f4ea383a70efceb09ef84bf0ed29b4
Matt Long <minnvik98151@gmail.com>
2014-03-14 03:42:05 -0400

    Trying out xml request, messy console logs to check

--- transit/transit.js
+++ transit/transit.js
@@ -1,6 +1,15 @@
 /*comment 1 for commit*/
 
+function initialize() {
+	navigator.geolocation.getCurrentPosition(makeMap);
+	findData();
+}
+
+
 function makeMap(position) {
+
+	//Why do I always get an error, saying it can't read latitude,
+	//even though it can because the map finds my location?
 
 	var mapOptions = {
 		center: new google.maps.LatLng(position.coords.latitude, 
@@ -12,9 +21,39 @@ function makeMap(position) {
 								  mapOptions);
 }
 
-function initialize() {
-	navigator.geolocation.getCurrentPosition(makeMap);
+
+function findData() {
+	var req = new XMLHttpRequest();
+	req.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+	console.log(req.readyState);
+	console.log(req.responseText);
+	req.onreadystatechange = dataReady(req);
+	req.send(null);
+	console.log(req.readyState);
+	console.log(req.responseText);
+	console.log(req.readyState);
+	console.log(req.responseText);
+	console.log(req.readyState);
+	console.log(req.responseText);
+
+}
+
+
+
+function dataReady(req) { 
+	if (req.readyState == 4 && req.status == 200) { 
+		trainSched = JSON.parse(req.responseText); 
+		tLine = trainSched.line; 
+		locateClosestStation(); 
+		markStations(); 
+		drawLines(); } 
+	else if (req.readyState == 4 && req.status == 500){ 
+		getTrains(); 
+	} 
+
+	console.log(req.responseText);
 }
 
 
 google.maps.event.addDomListener(window, 'load', makeMap);
+
