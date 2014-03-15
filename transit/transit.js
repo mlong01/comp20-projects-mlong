ca400d199e7f99040fc9e094947413105bb1f5d7
Matt Long <minnvik98151@gmail.com>
2014-03-15 04:39:44 -0400

    commented out problem section to return functionality to program

--- transit/transit.js
+++ transit/transit.js
@@ -71,10 +71,11 @@ function makeMarkers(map, myLoc, mylat, mylng) {
 	//function located at bottom of code so as not to make giant divide
 	var stations = makeStationArray();
 
-	var closest = findClosestStation(stations, mylat, mylng);
+	//var closest = findClosestStation(stations, mylat, mylng);
 
 	google.maps.event.addListener(meMarker, 'click', function() {
-		popup.setContent('<h5>LOCATION:</h5></br> <h6>('+mylat+', '+mylng+')</br></br><h5>CLOSEST STATION:</h5></br><h6>'+stations[closest[0]]['Station']+', '+closest[1]+' miles away</h6>');
+		//popup.setContent('<h5>LOCATION:</h5></br> <h6>('+mylat+', '+mylng+')</br></br><h5>CLOSEST STATION:</h5></br><h6>'+stations[closest[0]]['Station']+', '+closest[1]+' miles away</h6>');
+		popup.setContent('<h5>LOCATION:</h5></br> <h6>('+mylat+', '+mylng+')</br></br><h5>CLOSEST STATION:</h5></br><h6>TBD</h6>');
 		popup.open(map, meMarker);
 	})
 
@@ -84,7 +85,7 @@ function makeMarkers(map, myLoc, mylat, mylng) {
 }
 
 
-
+/*
 function findClosestStation(stations, mylat, mylng) {
 	var last = 0;
 	var minInd = START;
@@ -134,7 +135,7 @@ function calcDist(lat1, lon1, lat2, lon2) {
 function toRad(val) {
 	return val * Math.PI / 180;
 }
-
+*/
 
 function drawMarkers(map, stations) {
 	var last = 0;
