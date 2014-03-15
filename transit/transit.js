e83aac5f91138017c6d6d8ed032aefbe712f7238
Matt Long <minnvik98151@gmail.com>
2014-03-15 04:13:40 -0400

    found first typo

--- transit/transit.js
+++ transit/transit.js
@@ -114,13 +114,12 @@ function findClosestStation(stations, position) {
 
 
 function calcDist(lat1, lon1, lat2, lon2) {
-		
 
 	var R = 6371; // km
 	var dLat = toRad(lat2-lat1);
 	var dLon = toRad(lon2-lon1);
 	var lat1 = toRad(lat1);
-	var lat2 = toRad(;at2);
+	var lat2 = toRad(lat2);
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
 	        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
