33942a22f60594f305f0e87948f66119f80d6fc6
Matt Long <minnvik98151@gmail.com>
2014-03-15 00:35:26 -0400

    (hopefully) fixed issues with XMLHttpRequest

--- transit/transit.js
+++ transit/transit.js
@@ -1,8 +1,8 @@
 /*comment 1 for commit*/
 
 function initialize() {
-	navigator.geolocation.getCurrentPosition(makeMap);
 	findData();
+	navigator.geolocation.getCurrentPosition(makeMap);
 }
 
 
@@ -12,8 +12,13 @@ function makeMap(position) {
 	//even though it can because the map finds my location?
 
 	var mapOptions = {
+		//temp coded out to allow local testing
+		/*
 		center: new google.maps.LatLng(position.coords.latitude, 
 									   position.coords.longitude),
+		*/
+		center: new google.maps.LatLng(42.413, 
+									   -71.1067),
 		zoom: 12
 	};
 
@@ -22,38 +27,63 @@ function makeMap(position) {
 }
 
 
+
+//Given the technical difficulties with XML requests, JSON strings will be hard-
+//coded in for the time being
+
 function findData() {
 	var req = new XMLHttpRequest();
 	req.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
-	console.log(req.readyState);
-	console.log(req.responseText);
-	req.onreadystatechange = dataReady(req);
-	req.send();
-	console.log(req.readyState);
-	console.log(req.responseText);
-	console.log(req.readyState);
-	console.log(req.responseText);
-	console.log(req.readyState);
-	console.log(req.responseText);
-
+	req.onreadystatechange = dataReady;
+	req.send(null);
 }
 
 
 
 function dataReady(req) { 
+	// The readyState numbers:
+	// 0 = not initialized
+	// 1 = Set up
+	// 2 = Sent
+	// 3 = In progress
+	// 4 = Complete
+
 	if (req.readyState == 4 && req.status == 200) { 
 		trainSched = JSON.parse(req.responseText); 
-		tLine = trainSched.line; 
-		locateClosestStation(); 
-		markStations(); 
-		drawLines(); } 
+		tLine = trainSched['line']; 
+ 	} 
 	else if (req.readyState == 4 && req.status == 500){ 
-		getTrains(); 
+		alert("Data Retrieval Error - Please refresh page");
 	} 
 
-	console.log(req.responseText);
+	console.log(tLine);
 }
 
+/*
+function findData() {
+	station = 'red';
+
+	if(station == 'red') {
+		placeStations('red');
+	} else if (station == 'blue') {
+		placeStations('blue');
+	} else if (station == 'orange') {
+		placeStations('orange');
+	} else {
+		//handle error
+	}
+
+}
+*/
+function placeStations(station) {
+
+	parsed = JSON.parse(stations);
+	length = parsed.length;
+
+	console.log(stations);
+	console.log(length);
+
+}
 
 google.maps.event.addDomListener(window, 'load', makeMap);
 
